459ff37e8b0f5fb1d4191bf6adb3c78f0a4d8a17
KonScanner <kkombos@gmail.com>
2021-11-22 11:38:18 +0000

    Refactor part1

--- src/scrape_twitter.py
+++ src/scrape_twitter.py
@@ -4,26 +4,45 @@ import nest_asyncio
 from src.config import Config
 
 
-class TwitterScraper:
-    def __init__(self, username, limit=300,
+class TwitterScraper(Config):
+    def __init__(self, username=None,search=None, limit=300,
                  date_since=None, date_until=None):
+        super().__init__()
         self.username = username
+        self.search = search
         self.limit = limit
         nest_asyncio.apply()
         self.tweets = []
         self.out_path = "./data/" + f"{self.username}.csv"
-        self.scrape()
+        if username is not None:
+            self.scrape_user()
+        else:
+            self.scrape_search()
 
-    def scrape(self):
+    def scrape_user(self):
         c = twint.Config()
         c.Username = self.username
         c.Limit = self.limit
         c.Store_object = True
         c.Hide_output = True
+        c.Count = True
+        c.Pandas = True
         c.Store_csv = True
+        c.Custom_csv = Config.COLUMNS
         c.Output = self.out_path
         twint.run.Search(c)
-
-    def get_tweet_df(self):
-        df = pd.DataFrame(self.out_path)[Config.COLUMNS]
-        return df
+        self.df = twint.storage.panda.Tweets_df[Config.COLUMNS]
+        
+    def scrape_search(self):
+        c = twint.Config()
+        c.Search = self.search
+        c.Limit = self.limit
+        c.Store_object = True
+        c.Hide_output = True
+        c.Count = True
+        c.Pandas = True
+        c.Store_csv = True
+        c.Custom_csv = Config.COLUMNS
+        c.Output = self.out_path
+        twint.run.Search(c)
+        self.df = twint.storage.panda.Tweets_df[Config.COLUMNS_SEACH]
